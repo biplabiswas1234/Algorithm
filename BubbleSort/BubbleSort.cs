65b4c4df7f59e478ce0819bc77a8439c48ac2c8a
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-06-04 09:22:58 +0530

    [Biplab] Add: Anagram

--- BubbleSort/BubbleSort.cs
+++ BubbleSort/BubbleSort.cs
@@ -42,6 +42,31 @@ namespace BubbleSort
                 Console.WriteLine(i);
             }
         }
+        public void Anagram()
+        {
+            Console.WriteLine("Enter first string");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Enter second string");
+            string str2 = Console.ReadLine();
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+            Console.WriteLine("After sorting\n");
+            Console.WriteLine(ch1);
+            Console.WriteLine(ch2);
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            if (val1 == val2)
+            {
+                Console.WriteLine("Both the strings are Anagrams");
+            }
+            else
+            {
+                Console.WriteLine("Both the strings are not Anagrams");
+            }
+        }
     }
 }
 
--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -8,8 +8,9 @@ namespace BubbleSort
         {
             Console.WriteLine("Welcome to bubble sort");
             int[] a = { 4, 6, 3, 2, 5, 8, 8, 9 };
-            BubbleSorting bubblesort = new BubbleSorting();
-            bubblesort.BubbleSort();
+            BubbleSorting obj = new BubbleSorting();
+            obj.BubbleSort();
+            obj.Anagram();
         }
     }
 }
