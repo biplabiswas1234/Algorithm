26be12cae81939edb84e0bcfcf6822b01ff02c98
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-06-04 09:09:37 +0530

    [Biplab] Add: Bubble sort

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BubbleSort
+{
+    public class BubbleSorting
+    {
+        public void BubbleSort()
+        {
+            Console.WriteLine("Enter array size");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int temp = 0;
+            for (int i = 0; i < size; i++)
+            {
+                int flag = 0;
+
+                for (int j = 0; j < (size - i) - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        flag = 1;
+                    }
+
+                }
+                if (flag == 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("After bubble sort:\n");
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to bubble sort");
+            int[] a = { 4, 6, 3, 2, 5, 8, 8, 9 };
+            BubbleSorting bubblesort = new BubbleSorting();
+            bubblesort.BubbleSort();
+        }
+    }
+}
